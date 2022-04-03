8c0cc1707bdfd292031f164c6a283e45120fb006
KishorReddy524 <97526913+KishorReddy524@users.noreply.github.com>
2022-04-03 16:36:34 +0530

    Update db.js
    s

--- Express MVC/src/configs/db.js
+++ Express MVC/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectdb = ()=>{
-    return mongoose.connect("mongodb+srv://satishmvc:mvc123@cluster0.dg3uq.mongodb.net/mvc?retryWrites=true&w=majority");
+    return mongoose.connect("mongodb+srv://kishor:mvc524@cluster0.dg3uq.mongodb.net/mvc?retryWrites=true&w=majority");
 
 }
 module.exports=connectdb;
